04cc66574a717ac1a5a86c6e37104de4e550c1d3
Angel <angelcast42@gmail.com>
2019-04-03 03:59:22 -0600

    calculo de dias de vacaciones

--- src/app/components/rrhh/rrhh.component.ts
+++ src/app/components/rrhh/rrhh.component.ts
@@ -3,8 +3,10 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export interface Empleado { nombre: string; apellidos: string; fechaNacimiento: Date; dpi: number; sexo: string; EstadoCivil: string; Telefono: number; email: string;  FechaIngreso: Date;  emailLaboral: string;  cargo: string; jefeinmediato: string; departamento: string;}
+export interface Empleado { nombre: string; apellidos: string; fechaNacimiento: Date; dpi: number; sexo: string; EstadoCivil: string; Telefono: number; email: string;  FechaIngreso: Date;  emailLaboral: string;  cargo: string; jefeinmediato: string; departamento: string;daysUsed:number}
 export interface EmpleadoId extends Empleado { id: string; }
+import * as moment from 'moment';
+import { detectChanges } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-rrhh',
@@ -16,7 +18,6 @@ export class RrhhComponent implements OnInit {
   ModuloUserRRHHEditar = JSON.parse(localStorage.getItem('ModuloUserRRHHEditar',));
   private empleadoCollection: AngularFirestoreCollection<Empleado>;
   empleados: Observable<EmpleadoId[]>;
-  
   nuevoEmpleado: Empleado = {
     nombre: '',
     apellidos: '',
@@ -30,14 +31,17 @@ export class RrhhComponent implements OnInit {
     emailLaboral: '',
     cargo: '',
     jefeinmediato: '',
-    departamento:''
+    departamento:'',
+    daysUsed:null
   };
   editar = true;
-  query: string;
-
+  query: string;  
   docEmpleado: AngularFirestoreDocument<Empleado>;
   editEmpleado: Observable<Empleado>;
-  
+  //vacations
+  vacationVar=false
+  totalDays
+  selectedEmployee
   constructor(private readonly afs: AngularFirestore) {
 
     this.empleadoCollection = afs.collection<Empleado>('empleados');
@@ -61,6 +65,7 @@ export class RrhhComponent implements OnInit {
   }
 
   verEmpleado(empleado) {
+    this.selectedEmployee=empleado
     this.docEmpleado = this.afs.doc(`empleados/${empleado.id}`);
     this.editEmpleado = this.docEmpleado.valueChanges();
     console.log(this.editEmpleado);
@@ -82,7 +87,8 @@ export class RrhhComponent implements OnInit {
       emailLaboral: '',
       cargo: '',
       jefeinmediato: '',
-      departamento:''
+      departamento:'',
+      daysUsed:null
     };
   }
 
@@ -100,5 +106,34 @@ export class RrhhComponent implements OnInit {
 ButtonEditarCancelar(){
   this.editar = true;
 }
-
+setVacations(empleado){
+  if(this.vacationVar==false){
+    this.vacationVar=true
+    if(empleado.daysUsed==null){
+      this.afs.doc(`empleados/${this.selectedEmployee.id}`).set({
+        daysUsed:0
+      },{merge:true}).then(_=>{
+        console.log("update ok")
+      }).catch(error=>{
+        console.log("error:",error)
+      })
+      let dateCreated=moment(empleado.FechaIngreso)
+      let dateNow=moment()
+      let yearsWorked=dateNow.diff(dateCreated,"years",true)
+      this.totalDays=yearsWorked*15
+      this.totalDays=this.totalDays.toFixed(2)
+      }
+    else{
+       let dateCreated=moment(empleado.FechaIngreso)
+      let dateNow=moment()
+      let yearsWorked=dateNow.diff(dateCreated,"years",true)
+      this.totalDays=yearsWorked*15
+      this.totalDays=this.totalDays.toFixed(2)
+  
+    }
+  }
+  else{
+    this.vacationVar=false
+  }
+}
 }
