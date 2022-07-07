f8bbfe3c0175beb084314a73a46ecd05877da3a8
Miltonsms <milsmsramirezz@gmail.com>
2022-07-07 16:10:39 -0600

    actualizacion modulo de cliente institusional y facturacicon

--- src/app/components/cliente-industrial/cliente-industrial.component.ts
+++ src/app/components/cliente-industrial/cliente-industrial.component.ts
@@ -5,17 +5,13 @@ import { map } from 'rxjs/operators';
 
 export interface ClienteIndustrial {
   Entidad: string,
-  Departamento:string,
-  nombre:string,
   nit:string,
-  email:string,
-  movil: string,
   pbx:string,
-  Extension:string,
   Direccion:string,
-  Direccionbodega:string,
-  Observaciones:string
+  Observaciones:string,
+  Departamentos:any,
 }
+
 export interface  ClienteIndustrialId extends ClienteIndustrial { id: string; }
 @Component({
   selector: 'app-cliente-industrial',
@@ -23,7 +19,15 @@ export interface  ClienteIndustrialId extends ClienteIndustrial { id: string; }
   styles: []
 })
 export class ClienteIndustrialComponent implements OnInit {
-
+  departamentoItem={
+    NombreDepartamento:'',
+    Contacto:'',
+    email:'',
+    movil:'',
+    Extension:'',
+    DireccionDepartemeto:'',
+    Observaciones:'',
+  }
   //variables para permisos 
   ModuloUserClienteEliminar = JSON.parse(localStorage.getItem('ModuloUserClienteEliminar'));
   ModuloUserClienteEditar = JSON.parse(localStorage.getItem('ModuloUserClienteEditar',));
@@ -34,22 +38,29 @@ export class ClienteIndustrialComponent implements OnInit {
   //arrego para agregar nuevo cliente
   nuevoClienteIndustrial: ClienteIndustrial = {
     Entidad: " ",
-    Departamento:" ",
-    nombre:" ",
     nit:" ",
-    email:" ",
-    movil: " ",
     pbx:" ",
-    Extension:" ",
     Direccion:" ",
-    Direccionbodega:" ",
-    Observaciones:" "
+    Observaciones:" ",
+    Departamentos:[]
+  };
+  edit_cliente = {
+    Entidad: " ",
+    nit:" ",
+    pbx:" ",
+    Direccion:" ",
+    Observaciones:" ",
+    Departamentos:[]
   };
 
   editar = true;
   docCliente: AngularFirestoreDocument<ClienteIndustrial>;
   editCliente: Observable<ClienteIndustrial>;
   query: string;
+  p = 1;
+  itemsPerPage = 10;
+  idDepartamento:any
+
   constructor(private readonly afs: AngularFirestore) {
         // cliente
         this.clienteindustrialCollection = afs.collection<ClienteIndustrial>('clienteindus');
@@ -64,37 +75,49 @@ export class ClienteIndustrialComponent implements OnInit {
    }
 
   ngOnInit() {
+    
   }
   addCliente(cliente: ClienteIndustrial) {
+    this.nuevoClienteIndustrial.Departamentos.push(this.departamentoItem)
     this.clienteindustrialCollection.add(cliente);
     this.nuevoClienteIndustrial = {
       Entidad: " ",
-      Departamento:" ",
-      nombre:" ",
       nit:" ",
-      email:" ",
-      movil: " ",
       pbx:" ",
-      Extension:" ",
       Direccion:" ",
-      Direccionbodega:" ",
-      Observaciones:" "
+      Observaciones:" ",
+      Departamentos:[]
     };
+    this.departamentoItem={
+      NombreDepartamento:'',
+      Contacto:'',
+      email:'',
+      movil:'',
+      Extension:'',
+      DireccionDepartemeto:'',
+      Observaciones:'',
+    }
+  }
+  addDepartamento(){
+    this.edit_cliente.Departamentos.push(this.departamentoItem)
+    this.departamentoItem={
+      NombreDepartamento:'',
+      Contacto:'',
+      email:'',
+      movil:'',
+      Extension:'',
+      DireccionDepartemeto:'',
+      Observaciones:'',
+    }
+    this.docCliente.update(this.edit_cliente);
   }
   verCliente(cliente) {
-    // console.log(cliente);
+    console.log(cliente);
     this.docCliente = this.afs.doc(`clienteindus/${cliente.id}`);
-    console.log(this.docCliente,"docliente");
     this.editCliente=this.docCliente.valueChanges()
-    console.log("edit cliente",this.editCliente);
+    this.edit_cliente = cliente
+    console.log(cliente.Entidad)
     this.editar = true;
-  }
-  
-  ButtonEditar(){
-    this.editar = false
-}
-  ButtonEditarCancelar(){
-  this.editar = true;
   }
 
   //funcion para guardar cambios
@@ -105,5 +128,36 @@ export class ClienteIndustrialComponent implements OnInit {
   // eliminar clente
   daleteCliente() {
     this.docCliente.delete();
+  }
+  // eliminar Departamento
+  daleteDepartamento() {
+    this.edit_cliente.Departamentos.splice(this.idDepartamento, 1);
+    this.docCliente.update(this.edit_cliente);
+  }
+  ObtenerId(id){
+    this.idDepartamento=id
+  }
+  IditClient(item,id){
+    this.idDepartamento=id
+    this.departamentoItem={
+      NombreDepartamento:item.NombreDepartamento,
+      Contacto:item.Contacto,
+      email:item.email,
+      movil:item.movil,
+      Extension:item.Extension,
+      DireccionDepartemeto:item.DireccionDepartemeto,
+      Observaciones:item.Observaciones,
+    }
+  }
+  actualizarDepartamento(){
+    this.edit_cliente.Departamentos[this.idDepartamento].NombreDepartamento=this.departamentoItem.NombreDepartamento
+    this.edit_cliente.Departamentos[this.idDepartamento].Contacto=this.departamentoItem.Contacto
+    this.edit_cliente.Departamentos[this.idDepartamento].email=this.departamentoItem.email
+    this.edit_cliente.Departamentos[this.idDepartamento].movil=this.departamentoItem.movil
+    this.edit_cliente.Departamentos[this.idDepartamento].Extension=this.departamentoItem.Extension
+    this.edit_cliente.Departamentos[this.idDepartamento].DireccionDepartemeto=this.departamentoItem.DireccionDepartemeto
+    this.edit_cliente.Departamentos[this.idDepartamento].Observaciones=this.departamentoItem.Observaciones
+
+    this.docCliente.update(this.edit_cliente);
   }
 }
